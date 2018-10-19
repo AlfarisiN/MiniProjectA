034d8bddc25e46538934d7a24b3224cf2d4373e3
istiwiska <istiw28@gmail.com>
2018-10-19 16:15:53 +0700

    Splash Screen
    splash screen

--- app/src/main/java/com/android/mobilemarcom/MainActivity.java
+++ app/src/main/java/com/android/mobilemarcom/MainActivity.java
@@ -3,11 +3,11 @@ package com.android.mobilemarcom;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_main);
     }
 }
