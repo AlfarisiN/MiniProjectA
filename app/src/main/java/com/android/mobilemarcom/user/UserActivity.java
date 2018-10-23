e02522a74b1acb1fef20c4fd4c058b1edbfb6a1d
istiwiska <istiw28@gmail.com>
2018-10-23 17:18:53 +0700

    test
    test

--- app/src/main/java/com/android/mobilemarcom/user/UserActivity.java
+++ app/src/main/java/com/android/mobilemarcom/user/UserActivity.java
@@ -12,72 +12,102 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.SearchView;
 import android.widget.TableRow;
 
 import com.android.mobilemarcom.R;
 import com.android.mobilemarcom.adapters.UsersAdapter;
+import com.android.mobilemarcom.model.User;
+
+import java.util.ArrayList;
 
 public class UserActivity extends Fragment {
     private RecyclerView recyclerUser;
-    private UsersAdapter userListAdapter;
-    private EditText nameSearch;
+    private SearchView nameSearchUser;
     private TableRow rowlay;
-    private String[] EMP_NAMES = {
-            "Isti Wiska",
-            "Isti Wiska",
-            "Isti Wiska"
-    };
-    private String[] USERNAME = {
-            "IstiWiska",
-            "IstiWiska",
-            "IstiWiska"
-    };
-    private String[] ROLE = {
-            "Admin",
-            "Staff",
-            "Staff"
-    };
-    private String[] STATUS = {
-            "Aktif",
-            "Inaktif",
-            "Aktif"
-    };
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_user,container,false);
         recyclerUser = (RecyclerView) view.findViewById(R.id.recyclerUser);
-        nameSearch = (EditText) view.findViewById(R.id.nameSearchUser);
+        nameSearchUser = (SearchView) view.findViewById(R.id.nameSearchUser);
         rowlay = (TableRow) view.findViewById(R.id.rowlay);
 
         rowlay.setVisibility(View.GONE);
-        nameSearch.setOnClickListener(new View.OnClickListener() {
+
+
+        nameSearchUser.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onClick(View v) {
-                rowlay.setVisibility(View.VISIBLE);
-                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(),
-                        LinearLayout.VERTICAL, false);
-                recyclerUser.setLayoutManager(layoutManager);
-                tampilkanListUser();
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(nameSearchUser.getQuery().length()==0){
+                    rowlay.setVisibility(View.GONE);
+                    recyclerUser.setVisibility(View.GONE);
+                }else{
+                    final UsersAdapter adapter=new UsersAdapter(getContext(),getUsers());
+                    recyclerUser.setAdapter(adapter);
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(),
+                            LinearLayout.VERTICAL, false);
+                    recyclerUser.setLayoutManager(layoutManager);
+                    adapter.getFilter().filter(newText);
+                    rowlay.setVisibility(View.VISIBLE);
+                    recyclerUser.setVisibility(View.VISIBLE);
+                }
+                return false;
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return super.equals(obj);
             }
         });
-
-
 
         return view;
     }
 
+    private ArrayList<User> getUsers(){
+        ArrayList<User> users = new ArrayList<>();
+        User user = new User();
+        user.setId(1);
+        user.setEmp_name("Isti Wiska");
+        user.setUsername("istiwiska");
+        user.setRole("Admin");
+        user.setStatus("Aktif");
+        users.add(user);
 
-    private void tampilkanListUser(){
-        if(userListAdapter == null){
-            userListAdapter = new UsersAdapter(getContext(),EMP_NAMES,USERNAME,ROLE,STATUS);
-            recyclerUser.setAdapter(userListAdapter);
-        }
+        user = new User();
+        user.setId(2);
+        user.setEmp_name("Bagus Sujiwo");
+        user.setUsername("bagussujiwo");
+        user.setRole("Secretaris");
+        user.setStatus("Aktif");
+        users.add(user);
 
-        userListAdapter.notifyDataSetChanged();
+        user = new User();
+        user.setId(3);
+        user.setEmp_name("Muhammad Ghozi");
+        user.setUsername("muhammadghozi");
+        user.setRole("Bendahara");
+        user.setStatus("InAktif");
+        users.add(user);
+
+        user = new User();
+        user.setId(4);
+        user.setEmp_name("Bagus Sujiwo");
+        user.setUsername("bagussujiwo");
+        user.setRole("Secretaris");
+        user.setStatus("Aktif");
+        users.add(user);
+
+        return users;
+
+
     }
-
 
 
 }
