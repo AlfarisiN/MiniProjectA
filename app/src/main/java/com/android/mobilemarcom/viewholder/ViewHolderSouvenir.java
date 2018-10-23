326dcbf738911aa09118fc55c7c7c16e2a1a0fe0
AlfarisiN <31472669+AlfarisiN@users.noreply.github.com>
2018-10-23 23:11:59 +0700

    .

--- app/src/main/java/com/android/mobilemarcom/viewholder/ViewHolderSouvenir.java
+++ app/src/main/java/com/android/mobilemarcom/viewholder/ViewHolderSouvenir.java
@@ -1,23 +1,17 @@
 package com.android.mobilemarcom.viewholder;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-<<<<<<< HEAD
 import android.widget.PopupMenu;
-=======
->>>>>>> 0bbf3609c1d8c8437b634ab8633b8b3936ce3344
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.mobilemarcom.R;
 import com.android.mobilemarcom.model.ModelSouvenir;
-import com.android.mobilemarcom.souvenir.EditSouvenirActivity;
 
 public class ViewHolderSouvenir extends RecyclerView.ViewHolder {
     private TextView souvenir_list_code, souvenir_list_name, souvenir_list_quantity, souvenir_list_status;
@@ -36,11 +30,7 @@ public class ViewHolderSouvenir extends RecyclerView.ViewHolder {
     }
 
 
-<<<<<<< HEAD
     public void setModelSouvenir(final Context context, ModelSouvenir souvenir){
-=======
-    public void setModelSouvenir(Context context, ModelSouvenir souvenir){
->>>>>>> 0bbf3609c1d8c8437b634ab8633b8b3936ce3344
         //set Code
         String code = souvenir.getCode_souvenir();
         souvenir_list_code.setText(code);
@@ -60,7 +50,6 @@ public class ViewHolderSouvenir extends RecyclerView.ViewHolder {
         image_souvenir_option.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-<<<<<<< HEAD
                 PopupMenu popupMenu = new PopupMenu(context, image_souvenir_option);
                 popupMenu.getMenuInflater().inflate(R.menu.popup, popupMenu.getMenu());
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
@@ -79,9 +68,6 @@ public class ViewHolderSouvenir extends RecyclerView.ViewHolder {
                     }
                 });
                 popupMenu.show();
-=======
-
->>>>>>> 0bbf3609c1d8c8437b634ab8633b8b3936ce3344
             }
         });
     }
